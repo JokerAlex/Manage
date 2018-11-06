10ee61bc7c13b1627912b22d8f12d97c81beed84
JokerAlex <alex.zhao1023@gmail.com>
2018-11-06 19:43:02 +0800

    cors

--- src/main/java/com/dzhy/manage/security/SecurityConfig.java
+++ src/main/java/com/dzhy/manage/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.dzhy.manage.security.handler.UserAuthenticationFailureHandler;
 import com.dzhy.manage.security.handler.UserAuthenticationSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,8 +17,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /**
  * @ClassName SecurityConfig
@@ -66,11 +68,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(passwordEncoder());
     }
 
+    private UrlBasedCorsConfigurationSource getUrlBasedCorsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.setAllowCredentials(true);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setMaxAge(3600L);
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 // 由于使用的是JWT，我们这里不需要csrf
                 .csrf().disable()
+                .cors()
+                .configurationSource(getUrlBasedCorsConfigurationSource())
+                .and()
 
                 .exceptionHandling()
                 .authenticationEntryPoint(userAuthenticationEntryPoint)
@@ -82,6 +100,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilterBefore(userAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class)
 
                 .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                //开放链接
                 .antMatchers("/login").permitAll()
                 // swagger start 页面访问403错误
                 .antMatchers("/swagger-ui.html",
